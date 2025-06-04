708c239e481c036d0f20d8e48a740e2c01cdc487
Wiarr3 <wiora.biznes@gmail.com>
2025-06-05 00:50:41 +0200

    auth

--- src/main/java/com/smartass/server/config/SecurityConfig.java
+++ src/main/java/com/smartass/server/config/SecurityConfig.java
@@ -1,11 +1,16 @@
 package com.smartass.server.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtDecoders;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
@@ -15,7 +20,8 @@ public class SecurityConfig {
                         .pathMatchers("/actuator/prometheus", "/ws/**", "/alert/condition/**").permitAll()
                         .anyExchange().authenticated()
                 )
-                .csrf(ServerHttpSecurity.CsrfSpec::disable);
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .oauth2ResourceServer(ServerHttpSecurity.OAuth2ResourceServerSpec::jwt);
 
         return http.build();
     }
