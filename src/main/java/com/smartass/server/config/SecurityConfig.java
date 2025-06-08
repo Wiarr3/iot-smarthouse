34ea7e2497724ad0985a01b25c7037d3466d0358
Wiarr3 <wiora.biznes@gmail.com>
2025-06-08 18:23:58 +0200

    allow controller auth

--- src/main/java/com/smartass/server/config/SecurityConfig.java
+++ src/main/java/com/smartass/server/config/SecurityConfig.java
@@ -16,7 +16,7 @@ public class SecurityConfig {
     public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
         http
                 .authorizeExchange(exchanges -> exchanges
-                        .pathMatchers("/actuator/prometheus", "/ws/**", "/alert/condition/**").permitAll()
+                        .pathMatchers("/actuator/prometheus", "/ws/**", "/alert/condition/**", "/api/**").permitAll()
                         .anyExchange().authenticated()
                 )
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
