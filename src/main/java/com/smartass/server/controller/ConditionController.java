0dc98f0a9707a0d0f97bb5336d38aabda738d4b6
dkismerald <96881511+dkismerald@users.noreply.github.com>
2025-06-18 03:49:38 +0200

    db

--- src/main/java/com/smartass/server/controller/ConditionController.java
+++ src/main/java/com/smartass/server/controller/ConditionController.java
@@ -9,7 +9,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/alert/condition")
+@RequestMapping("/api/conditions")
 public class ConditionController {
     ConditionRegistry conditionRegistry;
 
